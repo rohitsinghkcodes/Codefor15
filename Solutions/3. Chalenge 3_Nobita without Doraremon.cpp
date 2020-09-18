648f486b4634cecedf59248e37dadcb5727b3c5f
Rohit Singh <conankmails@gmail.com>
2020-09-18 21:12:03 +0530

    saolution 3:NobitaWthoutDoraemon added

---
+++ Solutions/3. Chalenge 3_Nobita without Doraremon.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	vector<char> v;
+	for(char c:s)
+	{
+		if(c=='+')
+			continue;
+		else
+			v.push_back(c);	
+	}
+	sort(v.begin(),v.end());
+	auto itr=v.begin();
+	for(;itr<v.end();itr++)
+	{
+		if(itr!=v.begin()) cout<<'+';
+		cout<<*itr;
+	}
+	return 0;
+}
