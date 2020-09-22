1a8febac6103362a75778a8ac642e19dafd77098
Rohit Singh <conankmails@gmail.com>
2020-09-22 20:48:52 +0530

    Update commit 1

--- Solutions/DAY6 - DAY10/Challenge 7 solutions/7.1_Bag_of_candies.cpp
+++ Solutions/DAY6 - DAY10/Challenge 7 solutions/7.1_Bag_of_candies.cpp
@@ -7,7 +7,7 @@ int main()
     vector<int> v(4);
     for(int &it:v) cin>>it;
     sort(v.begin(),v.end());
-    if((v.at(0)+v.at(3)==(v.at(1)+v.at(2)))) cout<<"YES"<<endl;
+    if(((v.at(0)+v.at(3))==(v.at(1)+v.at(2))) or ((v.at(0)+v.at(1)+v.at(2))==v.at(3))) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
     return 0;
 }
