78aca814913614e312f66cddee45c3e09b547e9f
Rohit Singh <conankmails@gmail.com>
2020-10-21 15:40:18 +0530

    file updated

---
+++ Solutions/DAY11-DAY15/11.1_kthElement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int &it:v) cin>>it;
+    for(int &it:v)
+    {
+        
+    }
+    return 0;
+}
